3dca337ab887243e3ebc61b82478f7a46e0914f5
PraveenAndrew <84122399+PraveenAndrew@users.noreply.github.com>
2022-08-03 15:32:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title> Bioactivity [Bio2A&B]  ||  SPIHER </title> 
+       <meta name="keywords" content="plant,bioactivity,biomedical,leaves">
+       <meta name="description" content="This is a bioactivity plant inflammatory">
+       <meta name="author" content="Praveen Mohan Yuhan">
+       <!-- favicon -->
+       <link rel="shortcut icon" href="./img/plants-logo.png" type="image/x-icon">
+       <!-- css custom style -->
+       <link rel="stylesheet" href="./bio2a/css/style.css" type="text/css">
+       <!-- font awesome -->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+        <!-- sweet alert  -->
+      <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body class="background-img">
+       
+    <header>
+           <nav>
+                  <div class="clg-logo">
+                       <img src="./bio2a/img/college_logo.png" title="spiher" alt="serverPrblm" class="college-img">
+                  </div>s
+                  <div class="clg-place">
+                       <div class="clg-details">
+                              <p class="clg-name">st.peter's institute higher education and research chennai</p>
+                              <p class="clg-contact">avadi, 044-12345678</p>
+                              <p class="spiher">spiher bio2a</p>
+                       </div>
+                  </div>
+                  <div class="plant-logo">
+                        <img src="./bio2a/img/plants-logo.png" title="spiher" alt="serverPrblm" class="college-img">
+                  </div>
+           </nav>
+    </header>
+
+  
+
+     <section class="two-database">
+ 
+        <div class="bio2a"><a href="http://localhost/msc-projects/bio2a/index.php">a database of anti-inflammatory activity</a></div>
+        <div class="bio2b"><a href="http://localhost/msc-projects/bio2b/index.php">a database of anti-leishmaniasis activity</a></div>
+
+      </section>
+
+          <footer>
+                   <div class="footer-college">
+                          <p>&diams;&nbsp;st.peter's institute of higher education and research</p>
+                          <p>Avadi, Chennai-600 054, Tamil Nadu.</p>
+                          <div class="contact">
+                                 <span><i class="fa-solid fa-phone"></i> &nbsp; +91 44 2655 5448/3720/3746</span>
+                                 <span><i class="fa-solid fa-mobile-screen"></i> &nbsp; +91 9445638085</span>
+                                 <span><i class="fa-solid fa-at"></i> &nbsp; admissions@spiher.ac.in</span>
+                          </div>
+                     </div>
+                   <div class="footer-college">
+                          <p>&hearts;&nbsp;connect with St.Peter's Institute of Higher Education and Research.</p>
+                          <div class="socials-link">
+                            <a href=""><span><i class="fa-brands fa-facebook-square"></i></span></a>
+                             <a href=""><span><i class="fa-brands fa-youtube-square"></i></span></a>
+                             <a href=""><span><i class="fa-brands fa-twitter-square"></i></span></a>
+                             <a href=""><span><i class="fa-brands fa-linkedin"></i></span></a>
+                             <a href=""><span><i class="fa-brands fa-instagram-square"></i></span></a>
+                          </div>
+
+                   </div>
+                   <div class="footer-college">
+                          <p>&diams;&nbsp;Useful Links</p>
+                          <ul>
+                                 <a href=""><li>st.peter engineering</li></a>
+                                 <a href=""><li>deemed university</li></a>
+                                 <a href=""><li>sports</li></a>
+                                 <a href=""><li>students club</li></a>
+                                 <a href=""><li>Terms of Usage</li></a>
+                                 <a href=""><li>Privacy PolicyRefund Policy</li></a>
+                          </ul>
+                   </div>
+                   <div class="footer-bottom">
+                          &copy; spiher | designed by Praveen &starf; Mohan &#9787; Yuhan &starf; (msc(cs) 2020-2022 batch) 
+                   </div>
+          </footer>
+       
+</body>
+</html>
